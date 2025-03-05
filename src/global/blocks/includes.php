4a15e272ee79d0b274a5f2128051b6acf8141539
Alex Kostin <kostin.ak@mail.ru>
2025-03-06 02:36:31 +0600

    Внедрены эффекты фильтра для анимации на странице категорий при наведении мыши.

--- src/global/blocks/includes.php
+++ src/global/blocks/includes.php
@@ -1,14 +1,25 @@
 <?php
     include_once "configs/config.php";
+
+
+    function darkMode(){
+        if(isset($_COOKIE["dark"])){
+            if($_COOKIE["dark"] == "true"){
+                echo 'class="dark-theme"';
+            }
+        }
+    }
 ?>
 
 <script src="global/js/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/js-cookie@3.0.5/dist/js.cookie.min.js"></script>
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo Config::$APP_FAVICON;?>">
-<script>
+<!--<script>
     $(window).on('load', function() {
         if (Cookies.get('dark') == "true"){
             $("body").addClass("dark-theme").stop(true, true);
         }
     });
-</script>
+</script>-->
+
+<body <?php darkMode() ?></body>
